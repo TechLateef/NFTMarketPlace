49b4e85d7a08b2dc7f43e5bdcf0d756ab515ddd1
techlateef <techlateef@gmail.com>
2024-11-27 17:23:05 +0100

    Commit changes before running forge install

--- src/NftMarket.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.28;
-
-contract NFTMarket is ERC721, ERC721URIStorage, Ownable{
-    using Counters for Counters.counter;
-    
-    
-}
---
+++ src/contracts/NftMarket.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.28;
+
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "openzeppelin/contracts/access/Ownable.sol";
+import {Counters} from "openzeppelin-contracts/contracts/utils/Counters.sol";
+
+contract NFTMarket is ERC721, ERC721URIStorage, Ownable{
+    using Counters for Counters.counter;
+    
+    
+}
