b5cae47fecfd2659a994be5b32bce8874dc8a49d
techlateef <techlateef@gmail.com>
2024-11-28 15:33:50 +0100

    reset format

--- src/contracts/NftMarket.sol
+++ src/contracts/NftMarket.sol
@@ -10,28 +10,25 @@ contract NFTMarket is ERC721, ERC721URIStorage, Ownable, ReentrancyGuard {
     uint256 private _tokenIdCounter;
     uint256 private _totalSupply;
 
-    struct Sale{
+    struct Sale {
         uint256 price;
         address seller;
     }
 
-
-struct Auction {
-    address seller;
-    uint256 startingPrice;
-    uint256 highestBid;
-    address highestBidder;
-    uint256 endTime;
-    bool active;
-}
+    struct Auction {
+        address seller;
+        uint256 startingPrice;
+        uint256 highestBid;
+        address highestBidder;
+        uint256 endTime;
+        bool active;
+    }
 
     mapping(uint256 => Auction) private _auctions;
 
     mapping(uint256 => Sale) private _sales;
     mapping(uint256 => uint256) private _royalties;
     mapping(uint256 => address) private _creators;
-
-
 
     event AuctionStarted(uint256 tokenId, uint256 startingPrice, uint256 endTime);
     event NewBid(uint256 tokenId, address bidder, uint256 amount);
@@ -40,11 +37,8 @@ struct Auction {
     event NFTListed(uint256 tokenId, uint256 price, address seller);
     event NFTSold(uint256 tokenId, uint256 price, address buyer);
 
-
-
-
-    constructor() ERC721('Mklee', 'MKL') Ownable(msg.sender) {
-        _tokenIdCounter = 1;  // Initialize tokenId counter
+    constructor() ERC721("Mklee", "MKL") Ownable(msg.sender) {
+        _tokenIdCounter = 1; // Initialize tokenId counter
     }
 
     function safeMint(address _to, string memory uri) public {
@@ -52,7 +46,7 @@ struct Auction {
         _creators[tokenId] = msg.sender;
         _safeMint(_to, tokenId);
         _setTokenURI(tokenId, uri);
-         _totalSupply++;
+        _totalSupply++;
     }
 
     // Function to increment and return the current token ID
@@ -64,25 +58,24 @@ struct Auction {
         return _totalSupply;
     }
 
-function getAuction(uint256 _tokenId) public view returns (Auction memory) {
-    return _auctions[_tokenId];
-}
+    function getAuction(uint256 _tokenId) public view returns (Auction memory) {
+        return _auctions[_tokenId];
+    }
 
     function setRoyalty(uint256 _tokenId, uint256 _percentage) public {
-    require(ownerOf(_tokenId) == msg.sender, "Only the owner can set royalties.");
-    require(_percentage <= 10, "Royalty cannot exceed 10%.");
-    _royalties[_tokenId] = _percentage;
+        require(ownerOf(_tokenId) == msg.sender, "Only the owner can set royalties.");
+        require(_percentage <= 10, "Royalty cannot exceed 10%.");
+        _royalties[_tokenId] = _percentage;
     }
 
     function getRoyalty(uint256 _tokenId) public view returns (uint256) {
-    return _royalties[_tokenId];
+        return _royalties[_tokenId];
     }
 
     function _exists(uint256 _tokenId) internal view returns (bool) {
-    Sale memory sale = _sales[_tokenId];
-    return sale.price > 0;
+        Sale memory sale = _sales[_tokenId];
+        return sale.price > 0;
     }
-
 
     // Overriding supportsInterface to handle both ERC721 and ERC721URIStorage
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
@@ -96,17 +89,16 @@ function getAuction(uint256 _tokenId) public view returns (Auction memory) {
 
     //Function to list NFT for sale
     function listForSale(uint256 _tokenId, uint256 _price) public {
-        require(ownerOf(_tokenId) == msg.sender,  'You are not the owner of this NFT.');
+        require(ownerOf(_tokenId) == msg.sender, "You are not the owner of this NFT.");
         require(_sales[_tokenId].price == 0, "NFT is already listed for sale.");
         require(_price > 0, "Price must be greater than zero.");
         _sales[_tokenId] = Sale(_price, msg.sender);
-        
-        emit NFTListed(_tokenId, _price, msg.sender);
 
+        emit NFTListed(_tokenId, _price, msg.sender);
     }
 
     //Function to buy NFT
-    function buy(uint256 _tokenId) public payable nonReentrant{
+    function buy(uint256 _tokenId) public payable nonReentrant {
         Sale memory sale = _sales[_tokenId];
         require(_exists(_tokenId), "Token ID does not exist.");
         require(sale.price > 0, "This NFT is not for sale");
@@ -114,11 +106,10 @@ function getAuction(uint256 _tokenId) public view returns (Auction memory) {
         uint256 royalty = (sale.price * _royalties[_tokenId]) / 100;
         uint256 sellerAmount = sale.price - royalty;
 
-
-        (bool royaltySent, ) = _creators[_tokenId].call{value: royalty}("");
+        (bool royaltySent,) = _creators[_tokenId].call{value: royalty}("");
         require(royaltySent, "Royalty transfer failed");
 
-        (bool sellerSent, ) = sale.seller.call{value: sellerAmount}("");
+        (bool sellerSent,) = sale.seller.call{value: sellerAmount}("");
         require(sellerSent, "Seller payment transfer failed");
 
         //Transfer the NFT to the buyer
@@ -127,23 +118,20 @@ function getAuction(uint256 _tokenId) public view returns (Auction memory) {
         //Remove the sale listing
         delete _sales[_tokenId];
         emit NFTSold(_tokenId, sale.price, msg.sender);
-
     }
-
 
     //Function remove nft from market place
     function cancelSale(uint256 _tokenId) public {
         Sale memory sale = _sales[_tokenId];
-        require(sale.seller== msg.sender, "You are not the seller.");
+        require(sale.seller == msg.sender, "You are not the seller.");
         delete _sales[_tokenId];
     }
 
-
     //function to auction NFT
     function auctionNFT(uint256 _tokenId, uint256 _startingPrice, uint256 _duration) public {
-    require(ownerOf(_tokenId) == msg.sender,  'You are not the owner of this NFT.');
-    require(!_auctions[_tokenId].active, "Auction already active for this NFT.");
-    require(_startingPrice > 0, "Starting price must be greater than zero.");
+        require(ownerOf(_tokenId) == msg.sender, "You are not the owner of this NFT.");
+        require(!_auctions[_tokenId].active, "Auction already active for this NFT.");
+        require(_startingPrice > 0, "Starting price must be greater than zero.");
 
         //Transfer NFT to contract for escrow during auction
         _transfer(msg.sender, address(this), _tokenId);
@@ -158,125 +146,115 @@ function getAuction(uint256 _tokenId) public view returns (Auction memory) {
             active: true
         });
 
-        emit AuctionStarted(_tokenId, _startingPrice, block.timestamp +_duration);
+        emit AuctionStarted(_tokenId, _startingPrice, block.timestamp + _duration);
     }
 
     //Function to bid NFT
-    function placeBid(uint256 _tokenId) public payable nonReentrant{
-    Auction storage auction = _auctions[_tokenId];
-    require(auction.active, "No active auction for this NFT.");
-    require(block.timestamp < auction.endTime,"Auction has ended.");
-    require(msg.value > auction.startingPrice, "Bid must be higher than starting price");
-    require(msg.value > auction.highestBid, "Bid must be higher than the current highest bid.");
+    function placeBid(uint256 _tokenId) public payable nonReentrant {
+        Auction storage auction = _auctions[_tokenId];
+        require(auction.active, "No active auction for this NFT.");
+        require(block.timestamp < auction.endTime, "Auction has ended.");
+        require(msg.value > auction.startingPrice, "Bid must be higher than starting price");
+        require(msg.value > auction.highestBid, "Bid must be higher than the current highest bid.");
 
-    //Refund the previous highest bidder
-    if(auction.highestBid > 0) {
-        payable(auction.highestBidder).transfer(auction.highestBid);
+        //Refund the previous highest bidder
+        if (auction.highestBid > 0) {
+            payable(auction.highestBidder).transfer(auction.highestBid);
+        }
+
+        //Update auction with new highest bid
+        auction.highestBid = msg.value;
+        auction.highestBidder = msg.sender;
+
+        emit NewBid(_tokenId, msg.sender, msg.value);
     }
-
-    //Update auction with new highest bid
-    auction.highestBid = msg.value;
-    auction.highestBidder = msg.sender;
-
-    emit NewBid(_tokenId, msg.sender, msg.value);
-    }
-
-
 
     //Function to End Auction
     function endAuction(uint256 _tokenId) public {
-     Auction storage auction = _auctions[_tokenId];
-    require(auction.active, "No active auction for this NFT.");
-    require(block.timestamp >= auction.endTime, "Auction has not ended yet.");
+        Auction storage auction = _auctions[_tokenId];
+        require(auction.active, "No active auction for this NFT.");
+        require(block.timestamp >= auction.endTime, "Auction has not ended yet.");
 
-    //Transfer NFT to the highest bidder or back to the seller if no bids
-    if(auction.highestBidder != address(0)){
-        _transfer(address(this), auction.highestBidder, _tokenId);
-        payable(auction.highestBidder).transfer(auction.highestBid);
-    } else{
-        //Return NFT to the seller if no bids
-        _transfer(address(this), auction.seller, _tokenId);
+        //Transfer NFT to the highest bidder or back to the seller if no bids
+        if (auction.highestBidder != address(0)) {
+            _transfer(address(this), auction.highestBidder, _tokenId);
+            payable(auction.highestBidder).transfer(auction.highestBid);
+        } else {
+            //Return NFT to the seller if no bids
+            _transfer(address(this), auction.seller, _tokenId);
+        }
+
+        //Mark auction as inactive
+        auction.active = false;
+
+        emit AuctionEnded(_tokenId, auction.highestBidder, auction.highestBid);
     }
-
-    //Mark auction as inactive
-    auction.active = false;
-
-    emit AuctionEnded(_tokenId, auction.highestBidder, auction.highestBid);
-    }
-
 
     //Function to Cancel Auction
     function cancelAuction(uint256 _tokenId) public {
-    Auction storage auction = _auctions[_tokenId];
-    require(auction.active, "No active auction for this NFT.");
-    require(msg.sender == auction.seller, "Only the seller can cancel the auction.");
-    require(block.timestamp < auction.endTime, "Auction already ended.");
+        Auction storage auction = _auctions[_tokenId];
+        require(auction.active, "No active auction for this NFT.");
+        require(msg.sender == auction.seller, "Only the seller can cancel the auction.");
+        require(block.timestamp < auction.endTime, "Auction already ended.");
 
-    //Return NFT to the Seller
-    _transfer(address(this), auction.seller, _tokenId);
+        //Return NFT to the Seller
+        _transfer(address(this), auction.seller, _tokenId);
 
-    //Mark auction as inactive
-    auction.active = false;
-    emit AuctionCanceled(_tokenId);
-
+        //Mark auction as inactive
+        auction.active = false;
+        emit AuctionCanceled(_tokenId);
     }
-
 
     function getListedNFTs() public view returns (uint256[] memory) {
-    uint256 totalTokens = totalSupply();
-    uint256 listedCount = 0;
+        uint256 totalTokens = totalSupply();
+        uint256 listedCount = 0;
 
-    // Count the listed tokens
-    for (uint256 i = 1; i <= totalTokens; i++) {
-        if (_sales[i].price > 0) {
-            listedCount++;
+        // Count the listed tokens
+        for (uint256 i = 1; i <= totalTokens; i++) {
+            if (_sales[i].price > 0) {
+                listedCount++;
+            }
         }
-    }
 
-    // Create an array with the exact size
-    uint256[] memory tokensForSale = new uint256[](listedCount);
-    uint256 counter = 0;
+        // Create an array with the exact size
+        uint256[] memory tokensForSale = new uint256[](listedCount);
+        uint256 counter = 0;
 
-    for (uint256 i = 1; i <= totalTokens; i++) {
-        if (_sales[i].price > 0) {
-            tokensForSale[counter] = i;
-            counter++;
+        for (uint256 i = 1; i <= totalTokens; i++) {
+            if (_sales[i].price > 0) {
+                tokensForSale[counter] = i;
+                counter++;
+            }
         }
-    }
 
-    return tokensForSale;
+        return tokensForSale;
     }
-
 
     function getAuctions() public view returns (uint256[] memory) {
-    uint256 totalTokens = totalSupply();
-    uint256 auctionCount = 0;
+        uint256 totalTokens = totalSupply();
+        uint256 auctionCount = 0;
 
-    // Count the active auctions
-    for (uint256 i = 1; i <= totalTokens; i++) {
-        if (_auctions[i].active) {
-            auctionCount++;
+        // Count the active auctions
+        for (uint256 i = 1; i <= totalTokens; i++) {
+            if (_auctions[i].active) {
+                auctionCount++;
+            }
         }
+
+        // Create an array with the exact size
+        uint256[] memory activeAuctions = new uint256[](auctionCount);
+        uint256 counter = 0;
+
+        for (uint256 i = 1; i <= totalTokens; i++) {
+            if (_auctions[i].active) {
+                activeAuctions[counter] = i;
+                counter++;
+            }
+        }
+
+        return activeAuctions;
     }
 
-    // Create an array with the exact size
-    uint256[] memory activeAuctions = new uint256[](auctionCount);
-    uint256 counter = 0;
-
-    for (uint256 i = 1; i <= totalTokens; i++) {
-        if (_auctions[i].active) {
-            activeAuctions[counter] = i;
-            counter++;
-        }
-    }
-
-    return activeAuctions;
-}
-
-
-
-
-receive() external payable {}
-fallback() external payable {}
-
+    receive() external payable {}
+    fallback() external payable {}
 }
--- test/NftMarket.t.sol
+++ test/NftMarket.t.sol
@@ -1,122 +1,115 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.28;
+
 import {Test, console} from "forge-std/Test.sol";
 import "../src/contracts/NftMarket.sol";
 
-
-
 contract NFTMarketTest is Test {
- NFTMarket public nftMarket;
+    NFTMarket public nftMarket;
     address public mk = address(0x1);
     address public bob = address(0x2);
 
-function setUp() public{
-    
-    //Deploy NFTMarket contract
-    nftMarket = new NFTMarket();
-    //Mint an NFT for mk
-    vm.prank(mk);
-    nftMarket.safeMint(mk, "ipfs://metadata1");
-}
+    function setUp() public {
+        //Deploy NFTMarket contract
+        nftMarket = new NFTMarket();
+        //Mint an NFT for mk
+        vm.prank(mk);
+        nftMarket.safeMint(mk, "ipfs://metadata1");
+    }
 
-function testMint() public {
-    //verify totatl supply increases after minting
-    uint256 totalSupplyBefore = nftMarket.totalSupply();
-    vm.prank(mk);
-    nftMarket.safeMint(mk,"ipfs://metadata2");
+    function testMint() public {
+        //verify totatl supply increases after minting
+        uint256 totalSupplyBefore = nftMarket.totalSupply();
+        vm.prank(mk);
+        nftMarket.safeMint(mk, "ipfs://metadata2");
 
-    vm.prank(mk);
-    nftMarket.setRoyalty(1, 5);
+        vm.prank(mk);
+        nftMarket.setRoyalty(1, 5);
 
-    uint256 totalSupplyAfter = nftMarket.totalSupply();
+        uint256 totalSupplyAfter = nftMarket.totalSupply();
 
-    assertEq(totalSupplyAfter, totalSupplyBefore + 1, "Total supply did not increase");
-}
+        assertEq(totalSupplyAfter, totalSupplyBefore + 1, "Total supply did not increase");
+    }
 
-function testBuy() public{
-    //Mk lists his NFT for sale
-    vm.prank(mk);
-    nftMarket.listForSale(1,1 ether);
+    function testBuy() public {
+        //Mk lists his NFT for sale
+        vm.prank(mk);
+        nftMarket.listForSale(1, 1 ether);
 
-    //Bob buys the Nft
-    vm.prank(bob);
-    vm.deal(bob, 3 ether);
-    nftMarket.buy{value: 1 ether}(1);
-    assertEq(nftMarket.ownerOf(1), bob, "Owner should be Bob after purchase"); 
-}
+        //Bob buys the Nft
+        vm.prank(bob);
+        vm.deal(bob, 3 ether);
+        nftMarket.buy{value: 1 ether}(1);
+        assertEq(nftMarket.ownerOf(1), bob, "Owner should be Bob after purchase");
+    }
 
-function testAuction() public {
-    
-    //mk start an auction for his NFT
-    vm.prank(mk);
-    nftMarket.auctionNFT(1, 1 ether, 1 days);
+    function testAuction() public {
+        //mk start an auction for his NFT
+        vm.prank(mk);
+        nftMarket.auctionNFT(1, 1 ether, 1 days);
 
-    //check auction state
-    NFTMarket.Auction memory auction = nftMarket.getAuction(1);
+        //check auction state
+        NFTMarket.Auction memory auction = nftMarket.getAuction(1);
 
-    assertEq(auction.seller, mk, "Auction seller should be Alice");
-    assertEq(auction.startingPrice, 1 ether, "Starting price should be 1 ether");
-    assertEq(auction.highestBid, 0, "Highest bid should be 0 initially");
-    assertTrue(auction.active, "Auction should be active");
-}
+        assertEq(auction.seller, mk, "Auction seller should be Alice");
+        assertEq(auction.startingPrice, 1 ether, "Starting price should be 1 ether");
+        assertEq(auction.highestBid, 0, "Highest bid should be 0 initially");
+        assertTrue(auction.active, "Auction should be active");
+    }
 
-function testBid() public {
+    function testBid() public {
+        // Start auction
+        vm.prank(mk);
+        nftMarket.auctionNFT(1, 1 ether, 1 days);
 
-     // Start auction
-    vm.prank(mk);
-    nftMarket.auctionNFT(1, 1 ether, 1 days);
+        vm.prank(bob);
+        vm.deal(bob, 3 ether);
+        nftMarket.placeBid{value: 1.5 ether}(1);
 
-    vm.prank(bob);
-    vm.deal(bob, 3 ether);
-    nftMarket.placeBid{value: 1.5 ether}(1);
+        NFTMarket.Auction memory auction = nftMarket.getAuction(1);
 
+        assertEq(auction.highestBidder, bob, "Starting price should be 1 ether");
+        assertEq(auction.highestBid, 1.5 ether, "Highest bid should be 0 initially");
+        assertTrue(auction.active, "Auction should be active");
+    }
 
-    NFTMarket.Auction memory auction = nftMarket.getAuction(1);
+    function testEndAuction() public {
+        // Start auction
+        vm.prank(mk);
+        nftMarket.auctionNFT(1, 1 ether, 1 days);
 
-    assertEq(auction.highestBidder, bob, "Starting price should be 1 ether");
-    assertEq(auction.highestBid, 1.5 ether, "Highest bid should be 0 initially");
-    assertTrue(auction.active, "Auction should be active");
-}
+        vm.prank(bob);
+        vm.deal(bob, 3 ether);
+        nftMarket.placeBid{value: 1.5 ether}(1);
 
-function testEndAuction() public {
-     // Start auction
-    vm.prank(mk);
-    nftMarket.auctionNFT(1, 1 ether, 1 days);
+        // Fast-forward time to end the auction
+        vm.warp(block.timestamp + 2 days);
 
-    vm.prank(bob);
-    vm.deal(bob, 3 ether);
-    nftMarket.placeBid{value: 1.5 ether}(1);
+        vm.prank(mk);
+        nftMarket.endAuction(1);
 
-    // Fast-forward time to end the auction
-    vm.warp(block.timestamp + 2 days);  
+        assertEq(nftMarket.ownerOf(1), bob, "Owner should be Bob after auction ends");
+    }
 
-    vm.prank(mk);
-    nftMarket.endAuction(1);
+    function testCancelAuction() public {
+        // Start auction
+        vm.prank(mk);
+        nftMarket.auctionNFT(1, 1 ether, 1 days);
 
-    assertEq(nftMarket.ownerOf(1), bob,  "Owner should be Bob after auction ends");  
-}
+        vm.prank(mk);
+        nftMarket.cancelAuction(1);
 
-function testCancelAuction() public {
-     // Start auction
-    vm.prank(mk);
-    nftMarket.auctionNFT(1, 1 ether, 1 days);
+        assertEq(nftMarket.ownerOf(1), mk, "Owner should be seller");
+    }
 
-    vm.prank(mk);
-    nftMarket.cancelAuction(1);
+    function testNonSellerCannotEndAuction() public {
+        // Setup: Seller (mk) auctions an NFT
+        vm.prank(mk);
+        nftMarket.auctionNFT(1, 100 ether, 1 days); // Seller mk starts an auction
 
-    assertEq(nftMarket.ownerOf(1), mk, "Owner should be seller");
-}
-
-function testNonSellerCannotEndAuction() public {
-    // Setup: Seller (mk) auctions an NFT
-    vm.prank(mk);
-    nftMarket.auctionNFT(1, 100 ether, 1 days);  // Seller mk starts an auction
-
-    // Perform: Non-seller (bob) tries to cancel the auction
-    vm.prank(bob);  // Simulate calling the function from bob's address
-    vm.expectRevert("Only the seller can cancel the auction.");  // Expect revert message
-    nftMarket.cancelAuction(1);  // Non-seller should not be able to cancel the auction
-}
-
-
+        // Perform: Non-seller (bob) tries to cancel the auction
+        vm.prank(bob); // Simulate calling the function from bob's address
+        vm.expectRevert("Only the seller can cancel the auction."); // Expect revert message
+        nftMarket.cancelAuction(1); // Non-seller should not be able to cancel the auction
+    }
 }
